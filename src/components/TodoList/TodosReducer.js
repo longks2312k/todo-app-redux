3669ff9291096372ccbe80bf2f573762b748caa1
longks2312k <longks2312k@gmail.com>
2022-04-07 11:46:06 +0700

    1h-50p

---
+++ src/components/TodoList/TodosReducer.js
@@ -0,0 +1,19 @@
+const initState = [
+    { id: 1, name : 'Learn Yoga', completed: false, priority: 'Medium' },
+    { id: 2, name : 'Learn Redux', completed: false, priority: 'High' },
+    { id: 3, name : 'Learn React', completed: true, priority: 'Low' },
+]
+
+const todoListReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'todoList/addTodo':
+            return [
+                ...state,
+                action.payload
+                ]
+        default:
+            return state;
+    }
+}
+
+export default todoListReducer;
