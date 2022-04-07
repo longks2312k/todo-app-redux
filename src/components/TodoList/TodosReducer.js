f56d66fda89fc4b6c1e7cb044075b931f2519c8d
longks2312k <longks2312k@gmail.com>
2022-04-07 17:11:08 +0700

    Done Todo redux-core

--- src/components/TodoList/TodosReducer.js
+++ src/components/TodoList/TodosReducer.js
@@ -1,19 +1,22 @@
 const initState = [
-    { id: 1, name : 'Learn Yoga', completed: false, priority: 'Medium' },
-    { id: 2, name : 'Learn Redux', completed: false, priority: 'High' },
-    { id: 3, name : 'Learn React', completed: true, priority: 'Low' },
-]
+  { id: 1, name: "Learn Yoga", completed: false, priority: "Medium" },
+  { id: 2, name: "Learn Redux", completed: false, priority: "High" },
+  { id: 3, name: "Learn React", completed: true, priority: "Low" },
+];
 
 const todoListReducer = (state = initState, action) => {
-    switch(action.type){
-        case 'todoList/addTodo':
-            return [
-                ...state,
-                action.payload
-                ]
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "todoList/addTodo":
+      return [...state, action.payload];
+    case "todoList/toggleTodoStatus":
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+    default:
+      return state;
+  }
+};
 
 export default todoListReducer;
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,17 +1,17 @@
-import { Col, Row, Input, Button, Select, Tag } from 'antd';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTodo } from '../../redux/actions';
-import Todo from '../Todo';
-import {v4 as uuid4 } from 'uuid'
-import { todoRemainingSelector } from '../../redux/selector';
+import { Col, Row, Input, Button, Select, Tag } from "antd";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../../redux/actions";
+import Todo from "../Todo";
+import { v4 as uuid4 } from "uuid";
+import { todoRemainingSelector } from "../../redux/selector";
 
 export default function TodoList() {
-  const [todoName, setTodoName] = useState('')
-  const [priority, setPriority] = useState('Medium')
-  const dispatch = useDispatch()
+  const [todoName, setTodoName] = useState("");
+  const [priority, setPriority] = useState("Medium");
+  const dispatch = useDispatch();
 
-  const todoList = useSelector(todoRemainingSelector)
+  const todoList = useSelector(todoRemainingSelector);
 
   const handleAddBtnClick = () => {
     dispatch(
@@ -22,37 +22,49 @@ export default function TodoList() {
         completed: false,
       })
     );
-    setTodoName('');
+    setTodoName("");
     setPriority("Medium");
   };
   const handleInputChange = (e) => {
-    setTodoName(e.target.value)
-  }
+    setTodoName(e.target.value);
+  };
 
   const handlePriorityChange = (value) => {
-    setPriority(value)
-  }
+    setPriority(value);
+  };
 
   return (
-    <Row style={{ height: 'calc(100% - 40px)' }}>
-      <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
-        {todoList.map( todo =><Todo key={todo.id} name={todo.name} priority={todo.priority} completed={todo.completed}/>)}
+    <Row style={{ height: "calc(100% - 40px)" }}>
+      <Col span={24} style={{ height: "calc(100% - 40px)", overflowY: "auto" }}>
+        {todoList.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            name={todo.name}
+            priority={todo.priority}
+            completed={todo.completed}
+          />
+        ))}
       </Col>
       <Col span={24}>
-        <Input.Group style={{ display: 'flex' }} compact>
+        <Input.Group style={{ display: "flex" }} compact>
           <Input value={todoName} onChange={handleInputChange} />
-          <Select defaultValue="Medium" value={priority} onChange={handlePriorityChange}>
-            <Select.Option value='High' label='High'>
-              <Tag color='red'>High</Tag>
+          <Select
+            defaultValue="Medium"
+            value={priority}
+            onChange={handlePriorityChange}
+          >
+            <Select.Option value="High" label="High">
+              <Tag color="red">High</Tag>
             </Select.Option>
-            <Select.Option value='Medium' label='Medium'>
-              <Tag color='blue'>Medium</Tag>
+            <Select.Option value="Medium" label="Medium">
+              <Tag color="blue">Medium</Tag>
             </Select.Option>
-            <Select.Option value='Low' label='Low'>
-              <Tag color='gray'>Low</Tag>
+            <Select.Option value="Low" label="Low">
+              <Tag color="gray">Low</Tag>
             </Select.Option>
           </Select>
-          <Button type='primary' onClick={handleAddBtnClick}>
+          <Button type="primary" onClick={handleAddBtnClick}>
             Add
           </Button>
         </Input.Group>
